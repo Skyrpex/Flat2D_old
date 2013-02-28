c093a51b14d6dacf5d8cef3802879a2e7bad8eac
Cristian Pallarés <skyrpex@gmail.com>
2013-02-28 06:21:27 +0100

    Añade la QUndoStack a qApp

--- Application.cpp
+++ Application.cpp
@@ -1,9 +1,16 @@
 #include "Application.hpp"
+#include <QUndoStack>
 
 Application::Application(int argc, char **argv) :
-    QApplication(argc, argv)
+    QApplication(argc, argv),
+    m_undoStack(new QUndoStack(this))
 {
     setOrganizationName("Remacu");
     setApplicationName("Flat2D");
     setApplicationVersion("Alpha");
+}
+
+QUndoStack *Application::undoStack() const
+{
+    return m_undoStack;
 }
--- Application.hpp
+++ Application.hpp
@@ -8,10 +8,17 @@
 #endif
 #define qApp (static_cast<Application *>(QCoreApplication::instance()))
 
+class QUndoStack;
+
 class Application : public QApplication
 {
 public:
     Application(int argc, char **argv);
+
+    QUndoStack *undoStack() const;
+
+private:
+    QUndoStack *m_undoStack;
 };
 
 #endif // APPLICATION_HPP
