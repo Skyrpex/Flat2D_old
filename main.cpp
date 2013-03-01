0388321171243d70aa95bba0b10aa0c8715fff88
Cristian Pallarés <skyrpex@gmail.com>
2013-03-01 06:31:15 +0100

    Fuera código comentado

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include "Application.hpp"
 #include "MainWindow.hpp"
-//#include <progressbar.h>
 #include <QAction>
 
 int main(int argc, char *argv[])
 {
     Application a(argc, argv);
-
-//    Manhattan::ProgressBar t;
-//    t.setValue(40);
-//    t.show();
 
     MainWindow w;
     w.show();
